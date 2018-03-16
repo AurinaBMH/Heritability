76e68105a5aca095e088e3d7ae4e729832ac2564
Aurina Arnatkeviciute <aurina.arnatk@gmail.com>
2018-03-16 18:36:32 +1100

    scripts to check connectom eproperties and motion QC

--- connectomes/QCcheck/connectomeProperties.m
+++ connectomes/QCcheck/connectomeProperties.m
@@ -1,27 +1,77 @@
-cd ('data/connectomes')
+
 %% Define options
-parcellation = 'custom200'; % 'HCPMMP1' , 'custom200'; 
+parcellation = 'HCPMMP1'; % 'HCPMMP1' , 'custom200';
 tract = 'FACT';
 sift = 'SIFT2';
-doPlot = true; 
-threshold = 0.5; 
+doPlot = true;
+threshold = 0.5;
+WhatTypeNetwork = 'wu'; % 'wu' - weighted undirected; 'bu' - binary undirected;
+whatNullModel = 'randmio_und'; % 'randmio_und' - randomise topology; 'shuffleWeights' - keep topology, randomise weights.
+selectTwins = true;
 
 %-----------------------------------------------------------------
-% Arrange data 
+% Load general info and connectomes
 %-----------------------------------------------------------------
-% plot the histogram for the average matrix
+cd ('data/connectomes')
 Conn = load(sprintf('%sANDfslatlas20_acpc_%s_%s_standard_structnets.mat', parcellation, tract, sift));
 Length = load(sprintf('%sANDfslatlas20_acpc_%s_%s_length_structnets.mat', parcellation, tract, sift));
+cd ..
 
-coordinates = Conn.COG;
-connectomes = Conn.ADJS;
-distances = Length.ADJS;
+if selectTwins
+    cd ('general')
+    load('twinCovariates.mat')
+    
+    % make a list of IDs for twins
+    % check if both twins from a pair have diffusion data, if not, exclude a
+    % pair;
+    i=1;
+    for MZ=1:size(MZ_ID,1)
+        t1 = intersect(Conn.SUBS, MZ_ID(MZ,1));
+        t2 = intersect(Conn.SUBS, MZ_ID(MZ,2));
+        if isempty(t1) || isempty(t2)
+            remMZ(i) = MZ;
+            i=i+1;
+        end
+    end
+    % do same for DZ
+    j=1;
+    for DZ=1:size(DZ_ID,1)
+        t1 = intersect(Conn.SUBS, DZ_ID(DZ,1));
+        t2 = intersect(Conn.SUBS, DZ_ID(DZ,2));
+        if isempty(t1) || isempty(t2)
+            remDZ(j) = DZ;
+            j=j+1;
+        end
+    end
+    
+    % remove pairs if one of the twins is missing diffusion data
+    MZ_ID(remMZ,:) = [];
+    DZ_ID(remDZ,:) = [];
+    MZ_age(remMZ,:) = [];
+    DZ_age(remDZ,:) = [];
+    MZ_sex(remMZ,:) = [];
+    DZ_sex(remDZ,:) = [];
+    
+    twins = vertcat(MZ_ID(:), DZ_ID(:));
+    [~, twinIND] = intersect(Conn.SUBS, twins);
+    
+    coordinates = Conn.COG(twinIND);
+    connectomes = Conn.ADJS(twinIND);
+    distances = Length.ADJS(twinIND);
+else
+    coordinates = Conn.COG;
+    connectomes = Conn.ADJS;
+    distances = Length.ADJS;
+end
+
 
 %-----------------------------------------------------------------
 % Plot the average of histograms
 %-----------------------------------------------------------------
-avg_counts = averageHistogram(connectomes, doPlot); 
-
+avg_counts = averageHistogram(connectomes, doPlot);
+%-----------------------------------------------------------------
+% Arrange data
+%-----------------------------------------------------------------
 % calculate average distances between ROIs as an average of all subjects
 numNodes = size(coordinates{1},1);
 numSubj = size(coordinates,2);
@@ -44,46 +94,45 @@ end
 avDist = mean(dist,3);
 
 %-----------------------------------------------------------------
-% Make three versions of group matrices
+% Make three versions of group matrices - length, variance, consistency
 %-----------------------------------------------------------------
-% plot the histogram for the average matrix
-% create group matrices
 [groupAdj_variance, consist_var] = giveMeGroupAdj_variance(connectomes);
-[groupAdj_consistency, groupDist, consist_cons] = giveMeGroupAdj_consistency(connectomes, distances,threshold);groupAdj_maskLength = fcn_group_average(adjMatr,avDist,hemiid);
-% replace zeros with NaNs; 
+[groupAdj_consistency, groupDist, consist_cons] = giveMeGroupAdj_consistency(connectomes, distances,threshold);
 groupAdj_maskLength = fcn_group_average(adjMatr,avDist,hemiid);
-% replace zeros with NaNs; 
-adjMatr(adjMatr==0) = NaN; 
+% replace zeros with NaNs;
+adjMatr(adjMatr==0) = NaN;
 % take average across subjectsclose all
-meanAdj = nanmean(adjMatr,3); 
-groupAdj_length = meanAdj.*groupAdj_maskLength; 
-groupAdj_length(isnan(groupAdj_length)) = 0; 
-
-%-----------------------------------------------------------------
+meanAdj = nanmean(adjMatr,3);
+groupAdj_length = meanAdj.*groupAdj_maskLength;
+groupAdj_length(isnan(groupAdj_length)) = 0;
+% plot group connectomes
+figure;
+nice_cmap = [make_cmap('steelblue',50,30,0);flipud(make_cmap('orangered',50,30,0))];
+subplot(1,3,1); imagesc(log(groupAdj_length)); axis square; set(gcf,'color','w');colormap(nice_cmap); title('Group matrix - length');
+subplot(1,3,2); imagesc(log(groupAdj_variance)); axis square; set(gcf,'color','w');colormap(nice_cmap); title('Group matrix - variance');
+subplot(1,3,3); imagesc(log(groupAdj_consistency)); axis square; set(gcf,'color','w');colormap(nice_cmap); title('Group matrix - consistency');
+%---------------
+--------------------------------------------------
 % Plot degree distribution for each
 %-----------------------------------------------------------------
-deg_var = degrees_und(groupAdj_variance); 
-deg_cons = degrees_und(groupAdj_consistency); 
-deg_length = degrees_und(groupAdj_length); 
+deg_var = degrees_und(groupAdj_variance);
+deg_cons = degrees_und(groupAdj_consistency);
+deg_length = degrees_und(groupAdj_length);
 
-figure; 
-subplot(3,1,1); histogram(deg_var, 30, 'FaceColor', [1 .43 .29], 'EdgeColor', [.45 .45 .45]); title('Degree distribution variance-based group matrix'); xlabel('Degree, k'); 
-subplot(3,1,2); histogram(deg_cons, 30, 'FaceColor', [1 .43 .29], 'EdgeColor', [.45 .45 .45]); title('Degree distribution consistency-based group matrix'); xlabel('Degree, k'); 
-subplot(3,1,3); histogram(deg_length, 30, 'FaceColor', [1 .43 .29], 'EdgeColor', [.45 .45 .45]); title('Degree distribution distance-based group matrix'); xlabel('Degree, k'); 
+figure;
+subplot(3,1,1); histogram(deg_var, 50, 'FaceColor', [1 .43 .29], 'EdgeColor', [.45 .45 .45]); title('Degree distribution variance-based group matrix'); xlabel('Degree, k');
+subplot(3,1,2); histogram(deg_cons, 50, 'FaceColor', [1 .43 .29], 'EdgeColor', [.45 .45 .45]); title('Degree distribution consistency-based group matrix'); xlabel('Degree, k');
+subplot(3,1,3); histogram(deg_length, 50, 'FaceColor', [1 .43 .29], 'EdgeColor', [.45 .45 .45]); title('Degree distribution length-based group matrix'); xlabel('Degree, k');
 
 %-----------------------------------------------------------------
 % Calculate RC curves for group matrices
 %-----------------------------------------------------------------
-WhatTypeNetwork = 'wu'; % 'wu' - weighted undirected; 'bu' - binary undirected; 
-whatNullModel = 'randmio_und'; % 'randmio_und' - randomise topology; 'shuffleWeights' - keep topology, randomise weights. 
-
 RCcurves(groupAdj_length, WhatTypeNetwork,whatNullModel)
-title(sprintf('Group connectome - length %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel)); 
+title(sprintf('Group connectome - length %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel));
 RCcurves(groupAdj_variance, WhatTypeNetwork,whatNullModel)
-title(sprintf('Group connectome - variance %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel)); 
+title(sprintf('Group connectome - variance %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel));
 RCcurves(groupAdj_consistency, WhatTypeNetwork,whatNullModel)
-title(sprintf('Group connectome - consistency %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel)); 
-
+title(sprintf('Group connectome - consistency %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel));
 %-----------------------------------------------------------------
 % Calculate RC curves for individuals
 %-----------------------------------------------------------------
@@ -92,6 +141,34 @@ title(sprintf('Group connectome - consistency %s - %s - %s %s %s',parcellation, 
 %-----------------------------------------------------------------
 % Calculate correlations between individual connectomes
 %-----------------------------------------------------------------
-%[rall, pall] = connectomeCorrelation(parcellation, tract, sift, doPlot); 
+[rall, pall] = connectomeCorrelation(connectomes, doPlot);
 
+%-----------------------------------------------------------------
+% Plot where hubs are located in the connectome - plot all nodes and size
+% them according to the degree
+%-----------------------------------------------------------------
+
+coords = Conn.COG{1};
+
+figure; scatter3(coords(:,1),coords(:,2),coords(:,3), deg_var*5, [1 .43 .29], 'filled', 'MarkerEdgeColor','k');
+title ('Variance-based group matrix')
+figure;  scatter3(coords(:,1),coords(:,2),coords(:,3), deg_cons*5, [.64 .87 .93], 'filled', 'MarkerEdgeColor','k');
+title ('Consistency-based group matrix')
+figure;  scatter3(coords(:,1),coords(:,2),coords(:,3), deg_length*5, [0 .61 .49], 'filled', 'MarkerEdgeColor','k');
+title ('Length-based group matrix')
+
+%-----------------------------------------------------------------
+% Is there a RC in the left hemipshere?
+%-----------------------------------------------------------------
+
+groupAdj_varianceLC = groupAdj_variance(1:(numNodes-20)/2, 1:(numNodes-20)/2);
+groupAdj_lengthLC = groupAdj_length(1:(numNodes-20)/2, 1:(numNodes-20)/2);
+groupAdj_consistencyLC = groupAdj_consistency(1:(numNodes-20)/2, 1:(numNodes-20)/2);
+
+RCcurves(groupAdj_lengthLC, WhatTypeNetwork,whatNullModel)
+title(sprintf('Group connectome - length %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel));
+RCcurves(groupAdj_varianceLC, WhatTypeNetwork,whatNullModel)
+title(sprintf('Group connectome - variance %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel));
+RCcurves(groupAdj_consistencyLC, WhatTypeNetwork,whatNullModel)
+title(sprintf('Group connectome - consistency %s - %s - %s %s %s',parcellation, tract, sift, WhatTypeNetwork, whatNullModel));
 
---
+++ connectomes/QCcheck/motionQC.m
@@ -0,0 +1,167 @@
+
+% the effect of motion
+parcellation = 'HCPMMP1'; % 'HCPMMP1' , 'custom200';
+tract = 'FACT';
+sift = 'SIFT2';
+doPlot = true;
+threshold = 0.5;
+selectTwins = true;
+everySecond = false;
+
+
+cd ('data/connectomes')
+Conn = load(sprintf('%sANDfslatlas20_acpc_%s_%s_standard_structnets.mat', parcellation, tract, sift));
+Length = load(sprintf('%sANDfslatlas20_acpc_%s_%s_length_structnets.mat', parcellation, tract, sift));
+M = load('motion_parameters.mat');
+
+% calculate the average on b0 volumes
+load('Volume_index.mat')
+b0 = [1 17 33 49 65 81 96 113 129 145 161 177 192 209 225 241 257 273]; 
+[b0IND, a] = find(volume_ind==b0); 
+
+coordinates = Conn.COG;
+connectomes = Conn.ADJS;
+distances = Length.ADJS;
+motion = M.motion;
+
+numNodes = size(coordinates{1},1);
+numSubj = size(coordinates,2);
+numEdges = (numNodes*numNodes-numNodes)/2; 
+
+% calculate the average of motion
+avMotion = zeros(numSubj,2); 
+adj = zeros(numNodes, numNodes, numSubj); 
+avMotionb0 = zeros(numSubj,1); 
+secondIND = 1:2:576; 
+secondINDb0 = 1:2:36; 
+deg = zeros(numSubj,numNodes); 
+skew = zeros(numSubj,1); 
+density = zeros(numSubj,1); 
+strength = zeros(numSubj,1); 
+binAdj = zeros(numNodes, numNodes, numSubj); 
+maskAdj = zeros(numNodes, numNodes, numSubj); 
+edgeLength = zeros(numNodes, numNodes, numSubj); 
+edgesAdj = zeros(numSubj,numEdges); 
+
+for s=1:numSubj
+    
+    b0motion = motion{s}(b0IND,1); 
+   
+    if everySecond
+    allmotion = motion{s}(secondIND,:);
+    b0motion = b0motion(secondINDb0); 
+    else
+    allmotion = motion{s}; 
+    end
+    avMotionb0(s) = mean(b0motion);    
+    avMotion(s,:) = mean(allmotion,1); 
+    adj(:,:,s) = connectomes{s};
+    % calculate skewness for each subject to use it as "hubness"
+    deg(s,:) = degrees_und(connectomes{s}); 
+    density(s) = density_und(connectomes{s}); 
+    skew(s) = skewness(deg(s,:)); 
+    strength(s) = sum(nonzeros(connectomes{s})); 
+    binAdj(:,:,s) = logical(connectomes{s}); 
+    maskAdj(:,:,s) = maskuHalf(adj(:,:,s)); 
+    mask = ~isnan(maskAdj(:,:,s)); 
+    adjSubj = maskAdj(:,:,s); 
+    edgesAdj(s,:) = adjSubj(mask==1); 
+    edgeLength(:,:,s) = distances{s}; 
+    
+end
+
+b0motion = avMotion(b0IND,1); 
+figure; histogram(avMotion(:,1), 50); xlabel('motion - all volumes'); 
+figure; histogram(avMotionb0, 50); xlabel('motion - b0 volumes'); 
+
+% HOW CORRELATED MOTION ON B0 AND ALL VOLUMES; 
+[r] = corr(avMotion(:,1), avMotionb0, 'type', 'Spearman'); 
+figure; scatter(avMotion(:,1), avMotionb0); 
+xlabel('motion all volumes'); ylabel('motion b0 volumes')
+
+% corelate skewness with motion
+rskewb0 = corr(skew, avMotionb0, 'type', 'Spearman'); 
+rskew = corr(skew, avMotion(:,1), 'type', 'Spearman'); 
+
+figure; scatter(avMotionb0, skew); xlabel('motion - b0 volumes');  ylabel('Skewness'); 
+figure; scatter(avMotion(:,1), skew); xlabel('motion - all volumes');  ylabel('Skewness'); 
+
+% correlate density with motion
+rdensb0 = corr(density, avMotionb0, 'type', 'Spearman'); 
+rdens = corr(density, avMotion(:,1), 'type', 'Spearman'); 
+
+figure; scatter(avMotionb0, density); xlabel('motion - b0 volumes');  ylabel('Density'); 
+figure; scatter(avMotion(:,1), density); xlabel('motion - all volumes');  ylabel('Density'); 
+
+% correlate total strength of the connectome with motion
+rstrengthb0 = corr(strength, avMotionb0, 'type', 'Spearman'); 
+rstrength = corr(strength, avMotion(:,1), 'type', 'Spearman'); 
+
+figure; scatter(avMotionb0, strength); xlabel('motion - b0 volumes');  ylabel('Strength'); 
+figure; scatter(avMotion(:,1), strength); xlabel('motion - all volumes');  ylabel('Strength'); 
+
+% reproduce baum
+consistency = mean(binAdj,3);
+consistency = maskuHalf(consistency); 
+consistency(isnan(consistency)) = []; 
+
+[~, consistencyRoberts] = giveMeGroupAdj_variance(connectomes); 
+consistencyRoberts = maskuHalf(consistencyRoberts); 
+consistencyRoberts(isnan(consistencyRoberts)) = []; 
+
+% % for each edge correlate consistency with motion
+% rcons = zeros(length(consistency),1); 
+% rconsR = zeros(length(consistency),1); 
+% 
+% for edge = 1:length(consistency)
+%     rcons(edge) = corr(consistency, avMotion); 
+%     rconsR(edge) = corr(consistencyRoberts, avMotion); 
+% end
+% 
+% figure; histogram(rcons, 100); 
+% figure; histogram(rconsR, 100); 
+
+% for each edge correlate weight with motion
+rweight = zeros(length(consistency),1);
+pweight = zeros(length(consistency),1); 
+for edge = 1:numEdges
+    
+    [rweight(edge), pweight(edge)] = corr(edgesAdj(:,edge), avMotion(:,1), 'type', 'Spearman'); 
+  
+end
+
+% find proportion of significant correlations
+P = sum(pweight<0.05)/numEdges; 
+rweight(isnan(rweight)) = 0; 
+figure; histogram(rweight, 100); xlabel('correlation between weight and motion'); ylabel('number of edges'); 
+figure; histogram(pweight, 100); xlabel('correlation between weight and motion'); ylabel('number of edges'); 
+  
+% do with length
+% calulate length of each edge
+avLength = sum(edgeLength,3) ./ sum(edgeLength~=0,3); 
+avLength(isnan(avLength)) = 0;
+avLength = maskuHalf(avLength); 
+avLength = avLength(~isnan(avLength)); 
+
+nice_cmap = [make_cmap('steelblue',50,30,0);flipud(make_cmap('orangered',50,30,0))];
+% bin length into a hundred bins
+binLength = discretize(avLength,100); 
+cLength = zeros(length(binLength),3);
+for i=1:length(binLength)
+   
+    cLength(i,:) = nice_cmap(binLength(i),:);
+end
+figure; sz = 30; 
+scatter(consistency', rweight, sz,cLength, 'filled'); xlabel('Consistency'); ylabel('Motion effect'); title ('Length in color'); 
+
+% bin r into a hundred bins
+binR = discretize(rweight,100); 
+cR = zeros(length(binLength),3);
+for i=1:length(binLength)
+    cR(i,:) = nice_cmap(binR(i),:);
+end
+
+figure; sz = 30; 
+scatter(consistency', avLength, sz,cR, 'filled'); xlabel('Consistency'); ylabel('Length'); title ('Motion effect'); 
+
+
